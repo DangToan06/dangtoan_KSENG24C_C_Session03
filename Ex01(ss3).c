8b4de89cf99414c8615a426f51d551bd6730b993
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-21 16:21:28 +0700

    Update Ex01(ss3).c

--- Ex01(ss3).c
+++ Ex01(ss3).c
@@ -2,10 +2,13 @@
 
 
 int main(){
+	// B1: khai bao bien
 	char ten[20];
-	
+	// B2: nguoi dung nhap ten
 	scanf("%s", &ten);
+	// B3: in ra ket qua
 	printf("Xin chao %s", ten);
-
+	// B4: ket thuc chuong trinh
+	return 0;
 }
 
