01cb3e906e913c859a90aedd77447c7dff776c35
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-21 16:28:13 +0700

    Update Ex02(ss3).c

--- Ex02(ss3).c
+++ Ex02(ss3).c
@@ -6,11 +6,11 @@ int main(){
 	// B2: nhap nhiet do can doi
 	printf("Vui long nhap do Celsius: ");
 	scanf("%f", &C);
-	// B3: Xu ly doi tu tu do C sang do F
+	// B3: Xu ly doi tu do C sang do F
 	F = C * 9/5 + 32;
-	// 
+	// B4: In ra ket qua
 	printf("Doi sang do Fahrenheit la: %.2f", F);
-
+	// B5: ket thuc
 	return 0;
 }
 
