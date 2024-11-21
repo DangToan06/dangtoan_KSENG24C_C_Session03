a9335f31e41647942166c8ef5ca363f75686d404
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-21 16:25:06 +0700

    Update Ex02(ss3).c

--- Ex02(ss3).c
+++ Ex02(ss3).c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 int main(){
+	// B1: khai bao bien
 	float C, F;
+	// B2: nhap nhiet do can doi
 	printf("Vui long nhap do Celsius: ");
 	scanf("%f", &C);
-	
+	// B3: Xu ly doi tu tu do C sang do F
 	F = C * 9/5 + 32;
-	
+	// 
 	printf("Doi sang do Fahrenheit la: %.2f", F);
 
 	return 0;
