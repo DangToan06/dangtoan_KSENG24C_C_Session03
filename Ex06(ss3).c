cea2647afaf8d07bc0d69a8a41db70c3e9e6adfd
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-21 16:49:25 +0700

    Update Ex06(ss3).c

--- Ex06(ss3).c
+++ Ex06(ss3).c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
 int main(){
+	// B1: Khai bao bien
 	float canh, chieuCao;
-	printf("Vui long nhap canh và chieu cao hinh tam giac: ");
+	printf("Vui long nhap canh va chieu cao hinh tam giac: ");
+	// B2: Nhap chieu cao va canh tam giac
 	scanf("%f %f", &canh, &chieuCao);
+	// B3: tinh dien tich tam giac
 	float dienTich = (canh*chieuCao)/2;
+	// B4: In ket qua ra man hinh
 	printf("dien tich hinh tam giac la: %.1f", dienTich);
+	// B5: Ket thuc chuong trinh
 
 	return 0;
 }
