1ffc3f5835441d4e1870cf1743e1fc6c590255f9
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-21 16:37:38 +0700

    Update Ex03(ss3).c

--- Ex03(ss3).c
+++ Ex03(ss3).c
@@ -2,13 +2,18 @@
 #include <math.h>
 
 int main(){
+	// B1: khai bao bien
 	const float PI = 3.14;
 	float r;
+	// B2: Nhap ban kinh hinh tron
 	printf("Nhap vao ban kinh hinh tron: ");
 	scanf("%f", &r);
+	// B3: Tinh toan chu vi vaf dienj tich hinh tron
 	float chuVi = 2*PI*r;
 	float dienTich = PI*pow(r,2);
+	// B4: In ra ket qua
 	printf("chu vi hinh tron la %.2f\ndien tich hinh tron la %.2f", chuVi, dienTich);
+	// B5: ket thuc
 
 	return 0;
 }
