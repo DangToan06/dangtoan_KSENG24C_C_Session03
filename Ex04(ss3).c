2e9ac01407aa3dcdc00010fbb8c2a9b3b286c555
DangToan06 <dangtoan.2k6@gmail.com>
2024-11-21 16:44:00 +0700

    Update Ex04(ss3).c

--- Ex04(ss3).c
+++ Ex04(ss3).c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 
 int main(){
+	// B1: khai bao bien
 	float toan, van, anh;
 	printf("Nhap diem toan, van, anh: ");
+	// B2: Nhap diem toan van anh
 	scanf("%f %f %f", &toan, &van, &anh);
+	// B3: Tinh tong diem va diem trung binh
 	float tongDiem = toan + van + anh;
 	float diemTB = tongDiem/3;
-	
-	printf("tong diem l‡ %.2f \ndiem trung binh la: %.2f", tongDiem, diemTB);
-
+	// B4: In ra ket qua
+	printf("tong diem l√† %.2f \ndiem trung binh la: %.2f", tongDiem, diemTB);
+	// B5: Ket thuc chuong trinh
 	return 0;
 }
 
